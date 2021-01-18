3f9cacb2b3942208ec5e2e3ea3601da54432e369
Abigail G <dev@kb6.ee>
2021-01-18 01:41:02 -0500

    bump version to 1.0.0.post1

--- gridtools/__info__.py
+++ gridtools/__info__.py
@@ -6,7 +6,7 @@ __project__ = "gridtools"
 __summary__ = "A small Python library to convert between maidenhead grid locators and lat/long coördinates."
 __webpage__ = "https://gridtools.miaow.io/"
 
-__version__ = "1.0.0"
+__version__ = "1.0.0.post1"
 
 __author__ = "classabbyamp, 0x5c"
 __email__ = "dev@kb6.ee, dev@0x5c.io"
